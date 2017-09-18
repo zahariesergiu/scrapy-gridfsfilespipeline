9758dd0ba53bc03c5dbc80215deadc8fa86ed9c1
Sergiu <sergiu.zaharie@united-academics.org>
2017-09-18 17:53:24 +0300

    create media_downloaded to include file_guid, MongoDB objectid, filename in response

--- scrapy_gridfsfilespipeline/files.py
+++ scrapy_gridfsfilespipeline/files.py
@@ -1,10 +1,14 @@
 import datetime
+import time
 
 import gridfs
 import pymongo
 
-from scrapy.pipelines.files import FilesPipeline
+from scrapy.pipelines.files import FilesPipeline, logger, FileException
+from scrapy.utils.log import failure_to_exc_info
 from scrapy.utils.misc import md5sum
+from scrapy.utils.request import referer_str
+from twisted.internet import defer
 
 
 class GridFSFilesStorage(object):
@@ -57,8 +61,102 @@ class GridFSFilesPipeline(FilesPipeline):
     def media_to_download(self, request, info):
         """Override to include in the returned result mongo object id and file_guid instead of file_path and filename"""
 
+        def _onsuccess(result):
+            if not result:
+                return  # returning None force download
+
+            last_modified = result.get('last_modified', None)
+            if not last_modified:
+                return  # returning None force download
+
+            age_seconds = time.time() - last_modified
+            age_days = age_seconds / 60 / 60 / 24
+            if age_days > self.expires:
+                return  # returning None force download
+
+            referer = referer_str(request)
+            logger.debug(
+                'File (uptodate): Downloaded %(medianame)s from %(request)s '
+                'referred in <%(referer)s>',
+                {'medianame': self.MEDIA_NAME, 'request': request,
+                 'referer': referer},
+                extra={'spider': info.spider}
+            )
+            self.inc_stats(info.spider, 'uptodate')
+
+            checksum = result.get('checksum', None)
+            mongo_objectid = result.get('mongo_objectid', None)
+            filename = self.filename(request)
+            return {'url': request.url, 'file_guid': file_guid, 'checksum': checksum,
+                    'mongo_objectid': mongo_objectid, 'filename': filename}
+
+        file_guid = self.file_guid(request, info=info)
+        dfd = defer.maybeDeferred(self.store.stat_file, file_guid, info)
+        dfd.addCallbacks(_onsuccess, lambda _: None)
+        dfd.addErrback(
+            lambda f:
+            logger.error(self.__class__.__name__ + '.store.stat_file',
+                         exc_info=failure_to_exc_info(f),
+                         extra={'spider': info.spider})
+        )
+        return dfd
+
     def media_downloaded(self, response, request, info):
         """Override to include in the returned result mongo object id and file_guid instead of file_path and filename"""
+
+        referer = referer_str(request)
+
+        if response.status != 200:
+            logger.warning(
+                'File (code: %(status)s): Error downloading file from '
+                '%(request)s referred in <%(referer)s>',
+                {'status': response.status,
+                 'request': request, 'referer': referer},
+                extra={'spider': info.spider}
+            )
+            raise FileException('download-error')
+
+        if not response.body:
+            logger.warning(
+                'File (empty-content): Empty file from %(request)s referred '
+                'in <%(referer)s>: no-content',
+                {'request': request, 'referer': referer},
+                extra={'spider': info.spider}
+            )
+            raise FileException('empty-content')
+
+        status = 'cached' if 'cached' in response.flags else 'downloaded'
+        logger.debug(
+            'File (%(status)s): Downloaded file from %(request)s referred in '
+            '<%(referer)s>',
+            {'status': status, 'request': request, 'referer': referer},
+            extra={'spider': info.spider}
+        )
+        self.inc_stats(info.spider, status)
+
+        try:
+            file_guid = self.file_guid(request, response=response, info=info)
+            checksum, mongo_objectid = self.file_downloaded(response, request, info)
+        except FileException as exc:
+            logger.warning(
+                'File (error): Error processing file from %(request)s '
+                'referred in <%(referer)s>: %(errormsg)s',
+                {'request': request, 'referer': referer, 'errormsg': str(exc)},
+                extra={'spider': info.spider}, exc_info=True
+            )
+            raise
+        except Exception as exc:
+            logger.error(
+                'File (unknown-error): Error processing file from %(request)s '
+                'referred in <%(referer)s>',
+                {'request': request, 'referer': referer},
+                exc_info=True, extra={'spider': info.spider}
+            )
+            raise FileException(str(exc))
+
+        filename = self.filename(request)
+        return {'url': request.url, 'file_guid': file_guid, 'checksum': checksum, 'mongo_objectid': mongo_objectid,
+                "filename": filename}
 
     def file_downloaded(self, response, request, info):
         """Override to return also the mongo object id along with checksum"""
