8314bbf4d95736a4f3afa449447e6f8dedcffeb2
Sergiu <sergiu.zaharie@united-academics.org>
2017-09-21 10:54:45 +0300

    update setup.py: replace py_modules with packages

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='zahariesergiu@gmail.com',
     license='BSD',
     platforms=['Any'],
-    py_modules=['scrapy_gridfsfilespipeline'],
+    packages=['scrapy_gridfsfilespipeline'],
     install_requires=[
             'Scrapy',
             'pymongo',
